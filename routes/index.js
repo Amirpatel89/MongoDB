6149606bbe927d276ef1727c21d3910ded108179
Amir Patel <amirpatel89@gmail.com>
2018-01-03 11:54:18 -0500

    link db

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var mongodb = require('mongodb');
+var mongoClient = mongodb.MongoClient;
+const mongoUrl = 'mongodb://localhost:27017/moviesite';
+var db;
+mongoClient.connect(mongoUrl, (error, database)=>{
+	if(error){
+		throw error;
+	}else{
+		db = database;
+		console.log('connected to mongo')
+	}
 
+});
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	db.collection('movies').find().toArray((error, results)=>{
+		res.json(results);
+	})
+  
 });
 
 module.exports = router;
